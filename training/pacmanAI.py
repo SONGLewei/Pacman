f3bc6a03ff624127cb04541bf40f4243bb176710
El-lliam <2211305832@qq.com>
2025-03-16 02:47:40 +0100

    Basic of reseau neuron

--- training/pacmanAI.py
+++ training/pacmanAI.py
@@ -1,17 +1,63 @@
 import numpy as np
 
-
-class PacmanAI:
+"""
+    C'est le structure d'un AI individuel de Pacman se fait par Réseau Neuron
+    Il contien 3 couches:
+        Entrées : 
+        Hidden : 
+        Sortie : up down left right
+"""
+class PacmanOfReseauNeuron:
     def __init__(self):
-        self.input_size = 10 # les valeurs decision
-        self.hidden_size = 10 # hidden 
+        self.input_size = 16 # les valeurs decision
+        #self.hidden_size = 10 # hidden 
+        self.hidden_layers = [20,10]
         self.output_size = 4 # 4 direction 
-
+        """
         self.weights_input_to_hidden = np.random.uniform(-1, 1, (self.input_size, self.hidden_size))
         self.bias_hidden = np.random.uniform(-1, 1, (1, self.hidden_size))
         self.weights_hidden_to_output = np.random.uniform(-1, 1, (self.hidden_size, self.output_size))    
         self.bias_output = np.random.uniform(-1, 1, (1, self.output_size))
         self.fitness = 0
+        """
 
-    def decide_action():
-        return
+    def getDecision(self, input_vector):
+        action_probabilities = self.forward(input_vector)
+        direction_index = np.argmax(action_probabilities)
+        directions = ["UP", "DOWN", "LEFT", "RIGHT"]
+        return directions[direction_index]
+    
+    def relu(x):
+        return np.maximum(0,x)
+
+    def softmax(x):
+        e = np.exp(x)
+        return e//np.sum(e)
+
+    #INIT A NEURON NETWORK and set random value
+    def init_network(self):
+
+        # FOR each floor, creat matrix
+        layers = []
+        prev_dim = self.input_size
+
+        for h in self.hidden_layers:
+            layers.append(np.random.uniform(-1,1,size=(prev_dim,h)))
+            prev_dim = h
+        
+        # OUTPUT
+        layers.append(np.random.uniform(-1, 1, size=(prev_dim, self.output_size)))
+        return layers
+
+
+    def forward(self,network_weights, input_vector):
+        x = np.array(input_vector)
+        # HIDDEN
+        for idx, W in enumerate(network_weights[:-1]):
+            x = x.dot(W)
+            # Use relu in the hidden
+            x = self.relu(x)
+        # OUT
+        out = x.dot(network_weights[-1])
+        out = self.softmax(out)
+        return out
--- training/training_game.py
+++ training/training_game.py
@@ -1,26 +1,38 @@
+import pygame
 from src.controller.game import Game
-
+"""
+    The father is Game, we will use this to train the AI, UI will not be active.
+"""
 class TrainingGame(Game):
-    def __init__(self, ai_agent=None, max_steps=2000):
+    def __init__(self, ai_agent=None, max_steps=100000):
+
         super().__init__()
+
         self.ai_agent = ai_agent
         self.max_steps = max_steps
+
 
     def resetGameState(self):
         pass
 
     def run_once(self):
+        """
+        Without the UI run the game
+        """
         steps = 0
         self.isRunning = True
 
         while self.isRunning and steps < self.max_steps:
             steps += 1
+            # 让AI 决定走哪
             if self.ai_agent:
                 action = self.ai_agent.decide_action(self)
                 self.player.setDirection(action)
 
+            # Method of father
             super().update()
 
+            # win or lost
             if not self.isRunning:
                 break
 
@@ -31,9 +43,11 @@ class TrainingGame(Game):
         return self.score, victory
 
     def _is_dot(self, entity):
+        # 简化判断
         return entity.__class__.__name__ in ["Dot", "BigDot"]
 
     def render(self):
+        # empty
         pass
 
     def handleKeypress(self, event):
